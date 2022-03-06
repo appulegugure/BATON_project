63bf3062da4acca5503e97fe05da7ce817fd3da8
TA <appulegugure@gmail.com>
2022-03-06 16:29:36 +0900

    initial

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+// 接続に必要な情報を定数として定義
+define('DSN', 'mysql:host=db;dbname=BATON_main_database;charset=utf8');
+define('USER', 'BATON_user');
+define('PASSWORD', '123BGhj23jkL0');
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// 関数ファイルを読み込む
+require_once __DIR__ . '/functions.php';
+
+// データベースに接続
+$dbh = connect_db(); // 特にエラー表示がなければOK
+
+echo '<h1> -- BATON -- </h1>'
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<?php include_once __DIR__ . '/_header.html' ?>
+<body>
+    
+</body>
+</html>
