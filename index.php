7ced57505f9e8f58498b5c04c90c03209ca20e13
TA <appulegugure@gmail.com>
2022-03-06 16:34:39 +0900

    index.php修正

--- index.php
+++ index.php
@@ -6,13 +6,14 @@ require_once __DIR__ . '/functions.php';
 // データベースに接続
 $dbh = connect_db(); // 特にエラー表示がなければOK
 
-echo '<h1> -- BATON -- </h1>'
+
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
 <?php include_once __DIR__ . '/_header.html' ?>
 <body>
-    
+    <h1> -- BATON -- </h1>
+    <h5>エラーでてなければOK</h5>
 </body>
 </html>
