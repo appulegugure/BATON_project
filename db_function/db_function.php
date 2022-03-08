5513a098c0d975d0df7afce652517d43ebab9871
TA <appulegugure@gmail.com>
2022-03-08 16:10:41 +0900

    function_DB作成

--- db_function/db_function.php
+++ db_function/db_function.php
@@ -2,22 +2,18 @@
 
 require_once '../functions.php';
 
+//受注テーブルにデータ追加
 function create_oreder($adult,$child,$title,$day,$number_of_peaple_id,$price,$Contents,$Prerequisite)
 {
-    
-    // データベースに接続
     $dbh = connect_db();
-
     try {
         $dbh->beginTransaction();
 
-        // (4) データを登録するSQL
         $stmt1 = $dbh->prepare('INSERT INTO person_group(adult,child) VALUES (:adult,:child);');
         $stmt2 = $dbh->prepare('SET @LAST_ID_pg = LAST_INSERT_ID();');
         $stmt3 = $dbh->prepare('INSERT INTO order_ver_1(title,`day`,number_of_peaple_id,price,Contents,Prerequisite,community_id)
                                 VALUES (:title,:day,:number_of_peaple_id,:price,:contents,:Prerequisite,@LAST_ID_pg);');
 
-        // (5) 値をセット
         $stmt1->bindParam( ':adult', $adult, PDO::PARAM_INT);
         $stmt1->bindParam( ':child', $child, PDO::PARAM_INT);
         $stmt3->bindParam( ':title', $title, PDO::PARAM_STR);
@@ -36,25 +32,16 @@ function create_oreder($adult,$child,$title,$day,$number_of_peaple_id,$price,$Co
         }
 
     }catch(PDOException $e) {
-        
         echo $e->getMessage();
-        
         $dbh->rollBack();
-
     } finally {
-
-    // (10) データベースの接続解除
-    $dbh = null;
+        $dbh = null;
     }
 }
 
-
-
-
+//受注テーブル一覧表示
 function select_order_all_ALL()
 {
-    
-    // データベースに接続
     $dbh = connect_db();
     try {
         
@@ -64,13 +51,11 @@ function select_order_all_ALL()
         return $stmt1->fetchAll(PDO::FETCH_ASSOC);
     
     }catch(PDOException $e) {
-        
-        echo $e->getMessage();
-        
+        echo $e->getMessage();       
     }
 }
 
-
+//受注テーブル一覧をコミュニティーIDで絞って表示
 function select_order_all($community_id)
 {
     
@@ -80,30 +65,25 @@ function select_order_all($community_id)
         $stmt1 = $dbh->prepare("");
         $stmt1->execute();
         return $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    
+
     }catch(PDOException $e) {
-        
-        echo $e->getMessage();
-        
+        echo $e->getMessage();        
     }
 }
 
+//受注テーブルの詳細を表示
 function display_order($id)
 {
     
-    // データベースに接続
     $dbh = connect_db();
     try {
-        // (4) データを登録するSQL
-        $stmt1 = $dbh->prepare("
-                                SELECT 
-                                order_ver_1.title, 
-                                order_ver_1.`day`, 
-                                order_ver_1.Prerequisite,
-                                order_ver_1.Contents, 
-                                order_ver_1.number_of_peaple_id, 
-                                person_group.adult, 
-                                person_group.child 
+        $stmt1 = $dbh->prepare("SELECT  order_ver_1.title, 
+                                        order_ver_1.`day`, 
+                                        order_ver_1.Prerequisite,
+                                        order_ver_1.Contents, 
+                                        order_ver_1.number_of_peaple_id, 
+                                        person_group.adult, 
+                                        person_group.child 
                                 FROM order_ver_1 
                                 INNER JOIN person_group ON order_ver_1.community_id = person_group.id
                                 WHERE order_ver_1.id = $id;
@@ -113,64 +93,114 @@ function display_order($id)
 
         return $stmt1->fetchAll(PDO::FETCH_ASSOC);
     
-
     }catch(PDOException $e) {
-        // (8) エラーメッセージを出力
         echo $e->getMessage();
-        // (9) ロールバック
     }
 }
 
+//コミュニティ検索
 function search_community($input_word)
 {
     
     $dbh = connect_db();
     try {
 
-        $stmt1 = $dbh->prepare("");
+        $stmt1 = $dbh->prepare("SELECT community_name from community WHERE community_content LIKE '%$input_word%';");
         $stmt1->execute();
         return $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    
+
     }catch(PDOException $e) {
-        
+
         echo $e->getMessage();
-        
+
     }
 }
 
 function display_community($community_id)
 {
-    
+
     $dbh = connect_db();
     try {
 
         $stmt1 = $dbh->prepare("");
         $stmt1->execute();
         return $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    
+
     }catch(PDOException $e) {
-        
+
         echo $e->getMessage();
-        
+
     }
 }
 
-function create_community($a,$b,$c,$e,$d)
+function create_community($community_name,$community_maker,$conndition_1,$conndition_2,$conndition_3,$conndition_4,$conndition_5)
 {
-    
-    $dbh = connect_db();
-    try {
 
-        $stmt1 = $dbh->prepare("");
-        $stmt1->execute();
-        return $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    
+    $dbh = connect_db();
+
+    try {
+        $dbh->beginTransaction();
+
+        $stmt1 = $dbh->prepare('INSERT INTO community(community_name,community_maker,condition1,condition2,condition3,condition4,condition5) 
+                                VALUES (:community_name,:community_maker,:conndition_1,:conndition_2,:conndition_3,:conndition_4,:conndition_5);');
+
+        $stmt1->bindParam( ':community_name', $community_name, PDO::PARAM_STR);
+        $stmt1->bindParam( ':community_maker', $community_maker, PDO::PARAM_INT);
+        $stmt1->bindParam( ':conndition_1', $conndition_1, PDO::PARAM_STR);
+        $stmt1->bindParam( ':conndition_2', $conndition_2, PDO::PARAM_STR);
+        $stmt1->bindParam( ':conndition_3', $conndition_3, PDO::PARAM_STR);
+        $stmt1->bindParam( ':conndition_4', $conndition_4, PDO::PARAM_STR);
+        $stmt1->bindParam( ':conndition_5', $conndition_5, PDO::PARAM_STR);
+
+        $res1 = $stmt1->execute();
+
+        if( $res1 ) {
+            $dbh->commit();
+        }
+
     }catch(PDOException $e) {
-        
+
         echo $e->getMessage();
-        
+
+        $dbh->rollBack();
+
+    } finally {
+
+    // (10) データベースの接続解除
+    $dbh = null;
     }
 }
+
+function create_community_user($user_id,$community_id)
+{
+    $dbh = connect_db();
+    try {
+        $dbh->beginTransaction();
+
+        $stmt1 = $dbh->prepare("INSERT INTO community_user_test(user_test,community) 
+                                VALUES (:user_id_s,:community_id);");
+
+        $stmt1->bindParam( ':user_id_s', $user_id, PDO::PARAM_INT);
+        $stmt1->bindParam( ':community_id', $community_id, PDO::PARAM_INT);
+
+        $res1 = $stmt1->execute();
+
+        if( $res1 ) { 
+            $dbh->commit();
+        }
+
+    }catch(PDOException $e) {
+
+        echo $e->getMessage();
+
+        $dbh->rollBack();
+
+    } finally {
+
+    $dbh = null;
+    }
+}
+
 
 function create_order($a,$b,$c,$e,$d)
 {
@@ -206,20 +236,19 @@ function update_order_status($order_id,$user_id)
     }
 }
 
-
-function create_community_user($user_id)
-{
-    
+function community_all()
+{    
     $dbh = connect_db();
     try {
 
-        $stmt1 = $dbh->prepare("");
+        $stmt1 = $dbh->prepare("SELECT community_name from community;");
         $stmt1->execute();
         return $stmt1->fetchAll(PDO::FETCH_ASSOC);
-    
+
     }catch(PDOException $e) {
-        
+
         echo $e->getMessage();
-        
+
     }
 }
+
--- db_function/shousai.php
+++ db_function/shousai.php
@@ -2,4 +2,13 @@
 
 require_once __DIR__ . '/db_function.php';
 
-var_dump(display_order($_GET['order']));
+//var_dump(display_order($_GET['order']));
+
+//var_dump(create_community('tit',1,'tit','tit','tit','tit','tit'));
+
+create_community_user(5,2);
+
+echo 'search_community';
+
+var_dump(search_community('5'));
+
