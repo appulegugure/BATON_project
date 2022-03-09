14534c722eb50ff289525496042943b93434e267
TA <appulegugure@gmail.com>
2022-03-09 22:23:48 +0900

    受注テーブルにデータ追加

--- db_function.php
+++ db_function.php
@@ -2,30 +2,49 @@
 
 require_once __DIR__ . '/functions.php';
 
+
+//
 //受注テーブルにデータ追加
-function create_oreder($adult,$child,$title,$day,$number_of_peaple_id,$price,$Contents,$Prerequisite)
+function create_oreder($adult,$child,$order_user_email,$receive_user_email,$title,$job,$day,
+                    $price,$status,$condition1,$condition2,$condition3,$condition4,$condition5,$community_id)
 {
     $dbh = connect_db();
     try {
         $dbh->beginTransaction();
 
-        $stmt1 = $dbh->prepare('INSERT INTO person_group(adult,child) VALUES (:adult,:child);');
+        $stmt1 = $dbh->prepare('INSERT INTO Number_of_people(adult,child) VALUES (:adult,:child);');
         $stmt2 = $dbh->prepare('SET @LAST_ID_pg = LAST_INSERT_ID();');
-        $stmt3 = $dbh->prepare('INSERT INTO order_ver_1(title,`day`,number_of_peaple_id,price,Contents,Prerequisite,community_id)
-                                VALUES (:title,:day,:number_of_peaple_id,:price,:contents,:Prerequisite,@LAST_ID_pg);');
+        $stmt3 = $dbh->prepare('INSERT INTO job_order(order_user_email,receive_user_email,title,job,`day`,
+                                people_id,price,status,condition1,condition2,condition3,condition4,condition5,community_id)
+                                VALUES (:order_user_email,:receive_user_email,:title,:job,:day,
+                                @LAST_ID_pg,:price,:status,:con1,:con2,:con3,:con4,:con5,:community_id);');
 
         $stmt1->bindParam( ':adult', $adult, PDO::PARAM_INT);
         $stmt1->bindParam( ':child', $child, PDO::PARAM_INT);
+        $stmt3->bindParam( ':order_user_email', $order_user_email, PDO::PARAM_STR);
+        $stmt3->bindParam( ':receive_user_email', $receive_user_email, PDO::PARAM_STR);
         $stmt3->bindParam( ':title', $title, PDO::PARAM_STR);
+        $stmt3->bindParam( ':job', $job, PDO::PARAM_STR);
         $stmt3->bindParam( ':day', $day, PDO::PARAM_STR);
-        $stmt3->bindParam( ':number_of_peaple_id', $number_of_peaple_id, PDO::PARAM_INT);
+        //$stmt3->bindParam( ':number_of_peaple_id', $number_of_peaple_id, PDO::PARAM_INT);
         $stmt3->bindParam( ':price', $price, PDO::PARAM_INT);
-        $stmt3->bindParam( ':contents', $Contents, PDO::PARAM_STR);
-        $stmt3->bindParam( ':Prerequisite', $Prerequisite, PDO::PARAM_STR);
+        $stmt3->bindParam( ':status', $status, PDO::PARAM_STR);
+
+        $stmt3->bindParam( ':con1',$condition1, PDO::PARAM_STR);
+        $stmt3->bindParam( ':con2',$condition2, PDO::PARAM_STR);
+        $stmt3->bindParam( ':con3',$condition3, PDO::PARAM_STR);
+        $stmt3->bindParam( ':con4',$condition4, PDO::PARAM_STR);
+        $stmt3->bindParam( ':con5',$condition5, PDO::PARAM_STR);
+        $stmt3->bindParam( ':community_id', $community_id, PDO::PARAM_INT);
 
         $res1 = $stmt1->execute();
         $res2 = $stmt2->execute();
         $res3 = $stmt3->execute();
+
+        var_dump($res1);
+        var_dump($res2);
+        var_dump($res3);
+        
 
         if( $res1 && $res2 && $res3 ) {
             $dbh->commit();
@@ -38,6 +57,7 @@ function create_oreder($adult,$child,$title,$day,$number_of_peaple_id,$price,$Co
         $dbh = null;
     }
 }
+
 
 //受注テーブル一覧表示
 function select_order_all_ALL()
@@ -117,23 +137,29 @@ function display_community($community_id)
 }
 
 //コミュニティ作成
-function create_community($community_name,$community_maker,$conndition_1,$conndition_2,$conndition_3,$conndition_4,$conndition_5,$user_email)
+function create_community($community_name,$user_email,$conndition_1,$conndition_2,$conndition_3,$conndition_4,$conndition_5)
 {
     $dbh = connect_db();
     try {
         $dbh->beginTransaction();
-        $stmt1 = $dbh->prepare('INSERT INTO community(community_name,community_maker,condition1,condition2,condition3,condition4,condition5) 
-                                VALUES (:community_name,:community_maker,:conndition_1,:conndition_2,:conndition_3,:conndition_4,:conndition_5);');
+        $stmt1 = $dbh->prepare('INSERT INTO community(community_name,user_email,condition1,condition2,condition3,condition4,condition5,community_content) 
+                                VALUES (:community_name,:user_email,:conndition_1,:conndition_2,:conndition_3,:conndition_4,:conndition_5);');
+        $stmt2 = $dbh->prepare('SET @LAST_COM_ID = LAST_INSERT_ID();');
+        $stmt3 = $dbh->prepare("INSERT INTO community_user(community,user_email) 
+                                VALUES (@LAST_COM_ID,:user_email,FALSE);");
         $stmt1->bindParam( ':community_name', $community_name, PDO::PARAM_STR);
-        $stmt1->bindParam( ':community_maker', $community_maker, PDO::PARAM_INT);
+        $stmt1->bindParam( ':user_email', $user_email, PDO::PARAM_STR);
         $stmt1->bindParam( ':conndition_1', $conndition_1, PDO::PARAM_STR);
         $stmt1->bindParam( ':conndition_2', $conndition_2, PDO::PARAM_STR);
         $stmt1->bindParam( ':conndition_3', $conndition_3, PDO::PARAM_STR);
         $stmt1->bindParam( ':conndition_4', $conndition_4, PDO::PARAM_STR);
         $stmt1->bindParam( ':conndition_5', $conndition_5, PDO::PARAM_STR);
+        $stmt3->bindParam( ':user_email', $user_email, PDO::PARAM_STR);
         $res1 = $stmt1->execute();
-        
-        if( $res1 ) {
+        $res2 = $stmt2->execute();
+        $res3 = $stmt3->execute();
+
+        if( $res1 && $res2 && $res3) {
             $dbh->commit();
         }
     }catch(PDOException $e) {
@@ -170,7 +196,7 @@ function insert_community_user($community_id,$user_email)
 
 function create_order($a,$b,$c,$e,$d)
 {
-    
+
     $dbh = connect_db();
     try {
 
@@ -179,9 +205,9 @@ function create_order($a,$b,$c,$e,$d)
         return $stmt1->fetchAll(PDO::FETCH_ASSOC);
     
     }catch(PDOException $e) {
-        
+
         echo $e->getMessage();
-        
+
     }
 }
 
@@ -219,13 +245,13 @@ function select_community_all()
 }
 
 //コミュニティテーブルからコミュニティidで条件を絞って全絡むをセレクト
-function select_community_info($email){
+function select_community_info($id){
     $dbh = connect_db();
     try {
-        $stmt1 = $dbh->prepare("SELECT * FROM community WHERE user_email = :user_email;");
-        $stmt1->bindParam( ':user_email', $email, PDO::PARAM_STR);
+        $stmt1 = $dbh->prepare("SELECT * FROM community WHERE id = :id;");
+        $stmt1->bindParam( ':id', $id, PDO::PARAM_STR);
         $stmt1->execute();
-        return $stmt1->fetchAll(PDO::FETCH_ASSOC);
+        return $stmt1->fetch(PDO::FETCH_ASSOC);
     }catch(PDOException $e) {
         echo $e->getMessage();
     }
