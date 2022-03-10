9afcc9c9febf2a44c4f6e2a2f12058a746bb7ac1
TA <appulegugure@gmail.com>
2022-03-10 10:43:05 +0900

    orderスペル変更

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ echo '<he>';
 echo '<hr>';
 create_community('musukadai','appulegugure@gmail.com','1','2','3','4','5','content');
 echo '<hr>';
-create_oreder(1,2,'appulegugure@gmail.com','baton@gmail.com','safin','sea','2020/01/22,20',6,'reject','con1','con2','con3','con4','con5',1);
+create_order(1,2,'appulegugure@gmail.com','baton@gmail.com','safin','sea','2020/01/22,20',6,'reject','con1','con2','con3','con4','con5',1);
 echo '<hr>';
 ?>
 
