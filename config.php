79467758af17d94a239293a0ba137a5dc877393d
TA <appulegugure@gmail.com>
2022-03-12 10:04:35 +0900

    BATON_PATH

--- config.php
+++ config.php
@@ -4,5 +4,5 @@
 define('DSN', 'mysql:host=db;dbname=BATON_main_database;charset=utf8');
 define('USER', 'BATON_user');
 define('PASSWORD', '123BGhj23jkL0');
-define('BATON_path','/var/www/html/BATON');
+define('BATON_PATH','/var/www/html/BATON');
 
